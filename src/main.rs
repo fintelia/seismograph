b7075a48c6f64e2b1a802b6d940212b60cea0d55
Jonathan Behrens <fintelia@gmail.com>
2021-01-12 14:39:11 -0500

    Capture more trace data with every run

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
+use crate::run::Datapoint;
+use chrono::{DateTime, Utc};
 use indicatif::{ProgressBar, ProgressStyle};
 use serde::{Deserialize, Serialize};
 use std::{
+    collections::HashMap,
     fs::{self, File},
     io::BufWriter,
     path::PathBuf,
     sync::atomic::{AtomicBool, Ordering},
 };
 use structopt::StructOpt;
-use crate::run::Datapoint;
+use walkdir::WalkDir;
 
 mod run;
 mod sample;
@@ -26,7 +29,11 @@ struct TraceFile {
     hostname: String,
     kernel: String,
     cmdline: String,
+    profile: String,
+    experiment_start: DateTime<Utc>,
+    experiment_end: DateTime<Utc>,
     cpuinfo: String,
+    source_code: HashMap<PathBuf, String>,
     data: Vec<Datapoint>,
 }
 
@@ -55,14 +62,35 @@ fn main() {
         kernel: fs::read_to_string("/proc/version").unwrap(),
         cmdline: fs::read_to_string("/proc/cmdline").unwrap(),
         cpuinfo: fs::read_to_string("/proc/cpuinfo").unwrap(),
+        profile: if cfg!(debug_assertions) {
+            "debug"
+        } else {
+            "release"
+        }
+        .to_string(),
+        experiment_start: Utc::now(),
+        experiment_end: Utc::now(), // will be overwritten once the experiment ends
+        source_code: HashMap::new(),
         data: Vec::new(),
     };
+
+    for entry in WalkDir::new(format!("{}/src", env!("CARGO_MANIFEST_DIR")))
+        .into_iter()
+        .filter_map(Result::ok)
+        .filter(|e| e.file_type().is_file())
+    {
+        trace.source_code.insert(
+            entry.path().to_owned(),
+            fs::read_to_string(entry.path()).unwrap(),
+        );
+    }
 
     while trace.data.len() < MAX_DATAPOINTS && !EXIT.load(Ordering::SeqCst) {
         trace.data.push(run::single_iter());
         progress.set_position(trace.data.len() as u64);
         progress.set_length(progress.position() * 10000 / (1 + progress.position() % 10000));
     }
+    trace.experiment_end = Utc::now();
     progress.finish_at_current_pos();
 
     let writer = BufWriter::new(File::create(opt.output).unwrap());
